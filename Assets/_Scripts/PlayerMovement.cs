fabc53f9bb0f662eff282c057483bdb926546941
Emanuele Zignale <zignale92@gmail.com>
2025-06-03 15:47:19 +0200

    speed

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -1,11 +1,13 @@
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour{
+public class PlayerMovement : MonoBehaviour {
     [SerializeField] private float speed = 5.00f;
-
     void Update() {
         if (Input.GetKey(KeyCode.W))
             transform.Translate(0f, 0f, speed*Time.deltaTime);
     }
 }
+
+
+
