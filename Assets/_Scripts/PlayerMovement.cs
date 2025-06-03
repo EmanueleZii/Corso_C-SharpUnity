02f20abc6b25c915e7eddafae927349bb98c7bc2
Emanuele Zignale <zignale92@gmail.com>
2025-06-03 15:10:16 +0200

    keycode

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -1,20 +1,11 @@
 using Unity.VisualScripting;
 using UnityEngine;
 
-[]
-enum tasti
-{
-    w,
-    s,
-    a,
-    d
-}
-
 public class PlayerMovement : MonoBehaviour{
     [SerializeField] private float speed = 5.00f;
 
     void Update() {
-        if (Input.GetKey(tasti.w.ToString()))
+        if (Input.GetKey(KeyCode.W))
             transform.Translate(0f, 0f, speed*Time.deltaTime);
     }
 }
