3a41e95b1104f3371a4a7e571d17f3e94c9ecd34
Emanuele Zignale <zignale92@gmail.com>
2025-06-03 17:07:55 +0200

    esercizio1
    far muovere un object e far instanziare un oggetto a comando

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -1,13 +1,32 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
-    [SerializeField] private float speed = 5.00f;
-    void Update() {
-        if (Input.GetKey(KeyCode.W))
-            transform.Translate(0f, 0f, speed*Time.deltaTime);
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    private Vector3 movement;
+
+    //INSTATIATE
+    [SerializeField] private GameObject cubePrefab; 
+    [SerializeField] private Vector3 spawnPosition = new Vector3(0, 1, 0);
+
+
+    private void FixedUpdate()
+    {
+        movement = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W)) movement += Vector3.forward;
+        if (Input.GetKey(KeyCode.S)) movement += Vector3.back;
+        if (Input.GetKey(KeyCode.A)) movement += Vector3.left;
+        if (Input.GetKey(KeyCode.D)) movement += Vector3.right;
+
+        movement = movement.normalized * speed * Time.fixedDeltaTime;
+
+        // Applichiamo il movimento
+        transform.position += movement;
+
+        // Debug info (facoltativo)
+        Debug.Log($"Local Pos: {transform.localPosition} | World Pos: {transform.position}");
+        if (Input.GetKey(KeyCode.Space))
+            Instantiate(cubePrefab, spawnPosition, Quaternion.identity);
     }
 }
-
-
-
