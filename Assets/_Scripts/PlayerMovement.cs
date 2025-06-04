1c81e6a2bac10f89b99bb2176a7d2df64ee23982
Emanuele Zignale <zignale92@gmail.com>
2025-06-04 17:51:23 +0200

    esercizio 4

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -3,37 +3,42 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
+    private Vector3 moveDirection;
+    private Vector3 startPosition; 
+
+    void Start()
+    {
+        moveDirection = Vector3.zero;
+        startPosition = transform.position;
+    }
 
     void Update()
     {
-        Vector3 moveDirection = Vector3.zero;
+        moveDirection = Vector3.zero;
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            moveDirection += Vector3.forward; // avanti
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            moveDirection += Vector3.back; // indietro
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            moveDirection += Vector3.left; // sinistra
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            moveDirection += Vector3.right; // destra
-        }
+        if (Input.GetKey(KeyCode.W)) moveDirection += Vector3.forward;
+        if (Input.GetKey(KeyCode.S)) moveDirection += Vector3.back;
+        if (Input.GetKey(KeyCode.A)) moveDirection += Vector3.left;
+        if (Input.GetKey(KeyCode.D)) moveDirection += Vector3.right;
 
         transform.position += moveDirection * speed * Time.deltaTime;
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("ostacolo"))
+        {
+            moveDirection = Vector3.zero;
+            transform.position = startPosition; 
+            Debug.Log("hai perso non sei un campione");
+        }
+    }
+
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Portale"))
+        if (other.gameObject.CompareTag("traguardo"))
         {
-            speed *= 1;
-            Debug.Log("Collide!");
+            Debug.Log("sei un campione");
         }
     }
 }
