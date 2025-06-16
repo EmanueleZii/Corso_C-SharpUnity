5b315667d86dcad12a166ea91d3d411f5922c3de
Emanuele Zignale <zignale92@gmail.com>
2025-06-16 17:13:58 +0200

    Update Students.cs

--- Assets/_Scripts/UI/Students.cs
+++ Assets/_Scripts/UI/Students.cs
@@ -7,11 +7,14 @@ public class Students : MonoBehaviour
     public TMP_InputField inputField;
     public TextMeshProUGUI textPrefab;
     public Transform spawnParent;
-    public void SpawnText()
-    {
+    public void SpawnText() {
         string userInput = inputField.text;
         TextMeshProUGUI newText = Instantiate(textPrefab, spawnParent);
         newText.text = userInput;
     }
-    
+    public void ResetTesti() {
+        foreach (Transform child in spawnParent) {
+            Destroy(child.gameObject);
+        }
+    }
 }
